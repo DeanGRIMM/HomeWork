05ac40d3e3d78568ff9f9694410226353704e41c
Dmitry <dean.grimm@yandex.ru>
2023-01-06 15:36:21 +0800

    задача 4

---
+++ homework/задача 4/Program.cs
@@ -0,0 +1,19 @@
+﻿// задача 4 Напишите программу, которая принимает на вход 
+// три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите три числа");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int z = Convert.ToInt32(Console.ReadLine());
+int max = x;
+if (x>y && x>z)
+{
+    Console.WriteLine(max=x);
+}
+else if (y>x && y>z)
+{
+    Console.WriteLine(max=y);
+}
+else if (z>x && z>y)
+{
+    Console.WriteLine(max=z);
+}
