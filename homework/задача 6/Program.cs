538cbd3cdbc53a4408258ac73b5f335698411479
Dmitry <dean.grimm@yandex.ru>
2023-01-06 15:38:18 +0800

    задача 6

---
+++ homework/задача 6/Program.cs
@@ -0,0 +1,6 @@
+﻿// задача 6 Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка)
+Console.WriteLine("Введите целое число");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x%2==0) Console.WriteLine("yes");
+else Console.WriteLine("no");
