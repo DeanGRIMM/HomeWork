9e745463039f2c5fa49f1608c745a220c6917c74
Dmitry <dean.grimm@yandex.ru>
2023-01-06 15:34:43 +0800

    задача 2

---
+++ homework/задача 2/Program.cs
@@ -0,0 +1,14 @@
+﻿// задача 2 Напишите программу, которая на вход принимает два числа 
+// и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите целое число");
+int x = Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("Введите целое число");
+int y = Convert.ToInt32( Console.ReadLine());
+if (x>y)
+{
+    Console.WriteLine(x);
+}
+else
+{
+    Console.WriteLine(y);
+}
